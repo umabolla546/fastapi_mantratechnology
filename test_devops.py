0380cd3f498123f841398c2c9f1b03b8f5bdfbce
umabolla546 <60471967+umabolla546@users.noreply.github.com>
2025-08-05 14:38:05 +0530

    Update test_devops.py

--- test_devops.py
+++ test_devops.py
@@ -5,3 +5,23 @@ tesing
 import random
 
 print(random.randint(0,9))
+
+
+
+
+
+
+
+public class VowelConsonant {
+
+    public static void main(String[] args) {
+
+        char ch = 'i';
+
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' )
+            System.out.println(ch + " is vowel");
+        else
+            System.out.println(ch + " is consonant");
+
+    }
+}

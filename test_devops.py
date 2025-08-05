5a00922aa5ef86e4ad2cbc68755771dffa1becf6
umabolla546 <60471967+umabolla546@users.noreply.github.com>
2025-08-05 14:52:28 +0530

    Update test_devops.py

--- test_devops.py
+++ test_devops.py
@@ -25,3 +25,26 @@ public class VowelConsonant {
 
     }
 }
+
+
+
+output:
+public class VowelConsonant {
+
+    public static void main(String[] args) {
+
+        char ch = 'z';
+
+        switch (ch) {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+                System.out.println(ch + " is vowel");
+                break;
+            default:
+                System.out.println(ch + " is consonant");
+        }
+    }
+}
